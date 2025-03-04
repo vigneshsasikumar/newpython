eac32f17080dfce8fd65f9015de43bb817c06a55
vignesh <vigneshnayack77@gmail.com>
2025-03-05 00:27:04 +0530

    added new

--- pythonoperator.py
+++ pythonoperator.py
@@ -1,7 +1,60 @@
-first_name = 'Bro'
-food = 'pizza'
-email = 'svthemaster@shaolin.com'
+# input
+#import math
 
-print('Hello', first_name)
-print('Hello', first_name)
-print('Hello', first_name)
+#first_name = 'sv'
+#food = 'biriyani'
+#email = 'svthemaster@shaolin.com'
+
+#print('Hello', first_name)
+#print('sv like', food)
+#print('sv email is:', email)
+
+#arithmethic operator
+#books = 6
+#books = books + 1
+#books += 1
+#books -= 1
+#books = books * 3
+#books *= 3
+#books = books / 2
+#books /= 2
+#books = books ** 2
+#books **= 2
+#remainder = books % 2
+
+#print(remainder)
+
+#x = 2.15
+#y = 4
+#z = 6
+
+#result = round(x)
+#result = abs(y)
+#result = pow(5, 3)
+#result = max(x, y, z)
+#result = min(x, y, z)
+#print(result)
+
+#math
+#print(math.pi)
+#print(math.e)
+
+#x = 9.3
+
+#result = math.sqrt(x)
+#result = math.ceil(x)
+#result = math.floor(x)
+#print(result)
+
+#if statement
+
+age = 13
+
+if age >= 100:
+    print('you are too old to sign up')
+elif age >= 18:
+    print('your are now signed up!')
+elif age < 0:
+    print('your are not born yet')
+else:
+    print('you must be 18+ to sign up')
