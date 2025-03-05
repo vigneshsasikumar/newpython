7487f3e30fdaf9f88014c654dada0543db424db2
vignesh <vigneshnayack77@gmail.com>
2025-03-05 18:10:22 +0530

    added python casting

---
+++ python_casting.py
@@ -0,0 +1,41 @@
+# python casting
+
+# python casting is the process of converting the value of one type into value of another
+# int() , float(), str() - these are three datatypes present in the python casting
+
+# exammple1 casting the Integers
+# int() - using this integer datatype ->integer,float and string value will convert into integer data type.
+# It will remove the decimal points and place the value in the output
+
+
+x = int(4)
+y = int(7.7)
+z = int("2")
+
+print(x)
+print(y)
+print(z)
+
+
+#exammple2 casting Float
+# int() - using this float datatype ->integer,float and string value will convert into float data type.
+# It will add the decimal points and place the value in the output
+
+a = float(2)
+b = float(4.3)
+c = float("6")
+
+print(a)
+print(b)
+print(c)
+
+#example3 casting string
+# str() - using this string datatype -> integer,float and string value will convert into str data type.
+
+d = str("a2")
+e = str(5)
+f = str(4.0)
+
+print(d) 
+print(e)
+print(f)
